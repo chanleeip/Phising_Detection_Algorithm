e9372433bfbf30b412929c2f9b021d808b90dc99
chanleeip <chanleeip@gmail.com>
2023-12-24 23:32:57 +0530

    added cli

--- cli.py
+++ cli.py
@@ -3,22 +3,10 @@ import typer
 import os
 app=typer.Typer()
 
-@app.command(help="This cli is used for analyzing batch of emails/email.")
-def run_file(path:str='',dir:bool=False,file:bool=False):
+@app.command(help="This cli is used for analyzing batch of emails/email. ")
+def run_file(path:str=typer.Option('', "--path", "-p", help="add the path here")):
     if path:
         print(main.run_script(path))
-    if dir:
-        if os.path.isdir(path):
-            print("Sucess, analyzing")
-        else:
-            print("not a directory")
-
-    if file:
-        if os.path.exists(path):
-            print("Sucess, analyzing")
-        else:
-            print("not a file")
-
 
 if __name__ == "__main__":
     app()
