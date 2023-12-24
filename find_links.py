87b3bfe69e6cd8a7360dbf5fa03b0e18bae171e9
chanleeip <chanleeip@gmail.com>
2023-12-24 21:50:26 +0530

    whole algorithm working . need to add change calculation of marks and print the values in a .csv file

--- find_links.py
+++ find_links.py
@@ -1,30 +1,80 @@
 import os
-from email import policy
+from email import policy,message_from_string
 from email.parser import BytesParser
 from bs4 import BeautifulSoup
+import re
+from html import unescape
+from quopri import decodestring
 
 def read_file_and_find_links(eml_file_path):
     if not os.path.exists(eml_file_path):
         print(f"file doesnt exist")
         return
 
+    with open(eml_file_path, 'r') as file:
+        content = file.read()
+        html_match = re.search(r'<html\b[^>]*>.*?<\/html>',content,re.DOTALL | re.IGNORECASE)
+        # print(html_match)
+        if html_match is not None:
+            extracted_html = html_match.group(0)
+            soup = BeautifulSoup(extracted_html, 'html.parser')
+            href_tags = soup.find_all('a', href=True)
+            # print(href_tags)
+            href_values = [tag.get('href') for tag in href_tags]
+            return href_values
+     
+        
     with open(eml_file_path, 'rb') as file:
-        file_content = BytesParser(policy=policy.default).parse(file)
-        for part in file_content.iter_parts():
-            if part.get_content_type() == 'text/html':
-                html_content = part.get_content()
-                soup = BeautifulSoup(html_content, 'html.parser')
-                for a_tag in soup.find_all('a'):
-                    href_attribute = a_tag.get('href')
-                    return(href_attribute)
-                break  
-        else:
-            return("No HTML part found in the email body.")
+            file_content = BytesParser(policy=policy.default).parse(file)
+            for part in file_content.iter_parts():
+                if part.get_content_type() == 'text/html':
+                    html_content = part.get_content()
+                    soup = BeautifulSoup(html_content, 'html.parser')
+                    for a_tag in soup.find_all('a'):
+                        href_attribute = [a_tag.get('href')]
+                        return(href_attribute)
+                    break  
+            
+    with open(eml_file_path,'r')as file:
+        a_tags_match = re.findall(r'<a[^>]*\s*href\s*=\s*["\'](.*?)["\']', content, re.DOTALL | re.IGNORECASE)
+        return [a_tags_match]
+        # else:
+        #     extracted_html = html_match.group(0)
+        #     soup = BeautifulSoup(extracted_html, 'html.parser')
+        #     href_tags = soup.find_all('a', href=True)
+        #     href_values = [tag.get('href') for tag in href_tags]
+        #     return href_values  
+
+        # print(content)
+        # file_content = BytesParser(policy=policy.default).parse(file)
+        # href_values = [re.search(r'href="(.*?)"', str(tag)).group(0) for tag in href_tags]
+        # print(href_values)
+        # print(extracted_html)
+        # file_content = message_from_string(extracted_html, policy=policy.default)
+        # print(file_content)
+        # print(file_content.get_content_type())
+        # href_values = re.findall(r'<a[^>]*\s+href=["\'](.*?)["\'][^>]*>', extracted_html, re.IGNORECASE)
+        # print(href_values)
+        # print(file_content)
+        # for part in file_content.iter_parts():
+        #     print(part)
+        #     if part.get_content_type() == 'html':
+        #         html_content = part.get_content()
+        #         soup = BeautifulSoup(html_content, 'html.parser')
+        #         for a_tag in soup.find_all('a'):
+        #             href_attribute = a_tag.get('href')
+        #             print(href_attribute)
+        #             return(href_attribute)
+                   
+        #         break  
+        # else:
+        #     print("none")
+        #     return("No HTML part found in the email body.")
 
             
     
 
 
-file_path = '/Users/admin/Downloads/Phishing_Email _Samples/sample-13.eml'
+file_path = '/Users/admin/Downloads/Phishing_Email _Samples/sample-998.eml'
 
-# read_file_and_find_links(file_path)
+# print(read_file_and_find_links(file_path))
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import requests
 import json
 import find_spf
-import math
 import find_dkim_check
 import find_dkim_dmarc
 from urllib.parse import quote_plus
@@ -15,7 +14,8 @@ import find_from_email
 import find_sender_ip
 import find_smpts_sender_domain
 import os
-# all_files
+from tqdm import tqdm
+
 
 
 given_path = '/Users/admin/Downloads/Phishing_Email _Samples/sample-13.eml'
@@ -24,20 +24,23 @@ files_path={}
 if os.path.isdir(given_path):
         for filename in os.listdir(given_path):
                 file_path = os.path.join(given_path, filename)
-                files_dict[filename]=' '
+                files_dict[filename] = {"details": {"sender_id": {}, "links_present": [],"IP_adress_details":{},"IMTPS_IP_adress_details":{}}, "score": {}}
                 files_path[file_path]=" "
 elif os.path.isfile(given_path):
         files_dict[os.path.basename(given_path)]=" "
         files_path[given_path]=" "
+        files_dict = {os.path.basename(given_path): {"details": {"sender_id": {}, "links_present": []}, "score": {}} }
 else:
         print("Error")
 
 
 
-#Senders Email Address - (20 points)
-        #Higher is Better
+        ''' Senders Email Address - (20 points)
+                Higher is Better'''
+print(files_dict)
 for i in files_path.keys():
-        print(i)
+        tqdm.write("Processing Email Adress")
+        # print(i)
         email_id=find_from_email.read_files_and_find_from_email("/Users/admin/Downloads/Phishing_Email _Samples/sample-2222.eml")
         res = requests.get(f"https://emailvalidation.abstractapi.com/v1/?api_key={os.environ.get('EMAIL_VALIDATION')}&email={email_id}")
         response=json.loads(res.content)
@@ -53,101 +56,202 @@ for i in files_path.keys():
         "quality_score": float(response.get("quality_score")) * 10 
         }
 
-        overall_score = sum(scores[key] for key in scores.keys())
-        print(overall_score)
-        files_dict[os.path.basename(i)]=overall_score
+        if response["is_valid_format"]["value"]:
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["Valid_format"] = "Yes"
+        else:
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["Valid_format"] = "No"
+        if response["is_free_email"]["value"]:
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["Free-Email"] = "Yes"
+        else:
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["Free-Email"] = "No"
+        if response["is_disposable_email"]["value"] :
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["Is-Disposable-Email"] = "Yes"
+        else:
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["Is-Disposable-Email"] = "No"
+        if response["is_role_email"]["value"]:
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["Is-Role-Email"] = "Yes"
+        else:
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["Is-Role-Email"] = "No"
+        if response["is_catchall_email"]["value"] :
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["Is-Catchall-Email"] = "Yes"
+        else:
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["Is-Catchall-Email"] = "No"
+        if  response["is_smtp_valid"]["value"]:
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["SMTP-Valid"] = "Yes"
+        else:
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["SMTP-Valid"] = "No"
+        if  response["deliverability"]== "DELIVERABLE":
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["Deliverable"] = "Yes"
+        else:
+                files_dict[os.path.basename(i)]["details"]["sender_id"]["Deliverable"] = "No"
 
-# Links in Email - (25 points)
-#         Higher is better
+
+        overall_score = sum(scores[key] for key in scores.keys())
+        files_dict[os.path.basename(i)]["score"]=int(overall_score)
+
+        tqdm.write("10% Completed")   
+
+        '''Links in Email - (25 points) 
+                Higher is better'''
+
+        tqdm.write("Processing Links present in Email")
 
         urls=find_links.read_file_and_find_links(i)
-        print(urls)
-        encoded_url1=quote_plus(urls)
-        res=requests.get(f"https://www.ipqualityscore.com/api/json/url/{os.environ.get('URL_VALIDATION')}/{encoded_url1}")
-        response=(json.loads(res.content))
-        print(response)
-        scores = {
-        #    "success": 2 if response["success"] else -4,
-        #     "unsafe": -4 if response["unsafe"]else 2,
-        #     "dns_valid": 1 if response["dns_valid"]else -2,
-        #     "parking": 0 if response["parking"]else -2,
-        #     "spamming": -4 if response["spamming"]else 2,
-        #     "malware": -4 if response["malware"] else 2,
-        #     "phishing": -4 if response["phishing"] else 2,
-        #     "suspicious": -4 if response["suspicious"] else 2,
-                "risk_score":float(25-(response["risk_score"]/4))
-        }
-        overall_score = sum(scores[key] for key in scores.keys())
-        files_dict[os.path.basename(i)]=overall_score
+        for index,j in enumerate(urls):
+                # print(j)
+                # files_dict[os.path.basename(i)]["details"]["links_present"].append(j)
+                encoded_url1=quote_plus(j)
+                res=requests.get(f"https://www.ipqualityscore.com/api/json/url/{os.environ.get('URL_VALIDATION')}/{encoded_url1}")
+                response=(json.loads(res.content))
+                # print(response)
+                scores = {
+                #    "success": 2 if response["success"] else -4,
+                #     "unsafe": -4 if response["unsafe"]else 2,
+                #     "dns_valid": 1 if response["dns_valid"]else -2,
+                #     "parking": 0 if response["parking"]else -2,
+                #     "spamming": -4 if response["spamming"]else 2,
+                #     "malware": -4 if response["malware"] else 2,
+                #     "phishing": -4 if response["phishing"] else 2,
+                #     "suspicious": -4 if response["suspicious"] else 2,
+                        "risk_score":float(25-(response["risk_score"]/4))
+                }
 
+                if response["success"]:
+                        link_details={"URL":j,"Success":"Yes"}
+                        files_dict[os.path.basename(i)]["details"]["links_present"].append(link_details)
+                else:
+                        link_details={"URL":j,"Success":"No"}
+                        files_dict[os.path.basename(i)]["details"]["links_present"].append(link_details)
+                if response["unsafe"]:
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["Unsafe"]= "Yes"
+                else:
+                         files_dict[os.path.basename(i)]["details"]["links_present"][index]["Unsafe"]= "No"
+                if response["dns_valid"] :
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["DNS_valid"]= "Yes"
+                else:
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["DNS_valid"]= "No"
+                if response["parking"]:
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["Parking"] = "Yes"
+                else:
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["Parking"] = "No"
+                if response["spamming"]:
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["Spam"]= "Yes"
+                else:
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["Spam"] = "No"
+                if  response["malware"]:
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["Malware"] = "Yes"
+                else:
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["Malware"] = "No"
+                if  response["phishing"]:
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["Phishing"] = "Yes"
+                else:
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["Phishing"] = "No"
+                if  response["suspicious"]:
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["Suspicious"] = "Yes"
+                else:
+                        files_dict[os.path.basename(i)]["details"]["links_present"][index]["Suspicious"] = "No"
+                files_dict[os.path.basename(i)]["details"]["links_present"][index]["Risk_Score"] = response["risk_score"]
 
-# #SPF Record Check (15 points)
-#         #Higher is Better
+                overall_score = sum(scores[key] for key in scores.keys())
+                files_dict[os.path.basename(i)]["score"]=10
+                files_dict[os.path.basename(i)]["score"]=int(files_dict[os.path.basename(i)]["score"])+int(overall_score)
+
+                # tqdm.write("20% Completed") 
+# print(files_dict)
+        
+        '''SPF Record Check (15 points)
+                Higher is Better'''
+        tqdm.write("Checking SPF Record Check")
         status=find_spf.read_files_and_find_spf_record(i)
         if status=="Pass":
-                overall_score=15
+                overall_score=10
+                files_dict[os.path.basename(i)]["details"]["SPF_Record_Check"] = "Pass"
         else:
-                overall_score=0
+                overall_score=-10
+                files_dict[os.path.basename(i)]["details"]["SPF_Record_Check"]= "Fail/Error"
 
-        files_dict[os.path.basename(i)]=overall_score
-#DKIM CHECK (15 points)
-        #Higher is Better
+        files_dict[os.path.basename(i)]["score"]=10
+        files_dict[os.path.basename(i)]["score"]=int(files_dict[os.path.basename(i)]["score"])+int(overall_score)
+        tqdm.write("30% Completed") 
+
+        '''DKIM CHECK (15 points)
+                #Higher is Better'''
+        tqdm.write("Checking DKIM Record ")
         status=find_dkim_check.read_file_and_find_dkim_check(i)
         if status=="Pass":
                 overall_score=15
+                files_dict[os.path.basename(i)]["details"]["DKIM_check"] = "Pass"
         else:
                 overall_score=0
+                files_dict[os.path.basename(i)]["details"]["DKIM_check"] = "Fail/Error"
 
-        files_dict[os.path.basename(i)]=overall_score
+        files_dict[os.path.basename(i)]["score"]=int(files_dict[os.path.basename(i)]["score"])+int(overall_score)
+        tqdm.write("40% Completed") 
         
-#DMARC CHECK (10 points)
-        #HIgher is Better
+        '''DMARC CHECK (10 points)
+                HIgher is Better'''
+        tqdm.write("Checking DMARC Record ")
         status=find_dkim_dmarc.read_files_and_find_dkim_check(i)
         if status=="Pass":
                 overall_score=10
+                files_dict[os.path.basename(i)]["details"]["DMARC_check"] = "Pass"
         else:
                 overall_score=0
+                files_dict[os.path.basename(i)]["details"]["DMARC_check"] = "Fail/Error"
 
-        files_dict[os.path.basename(i)]=overall_score
+        files_dict[os.path.basename(i)]["score"]=int(files_dict[os.path.basename(i)]["score"])+int(overall_score)
+        tqdm.write("50% Completed") 
         
-#Content Analysis (25 points)
-        #Higher is Better
+        '''Content Analysis (25 points)
+                #Higher is Better'''
+        tqdm.write("Checking Content of the Email for any Phishing/Scam Keywords")
         content=find_email_body.extract_body_from_eml(i)
         found_keywords = [keyword for keyword in keywords["phishing_keywords"] if keyword in content]
 
         if found_keywords:
                 overall_score=10
+                files_dict[os.path.basename(i)]["details"]["Any_Phishing_Keyword_Found"] = "Yes"
         else:
                 overall_score=0
-        files_dict[os.path.basename(i)]=overall_score
+                files_dict[os.path.basename(i)]["details"]["Any_Phishing_Keyword_Found"] = "No"
+        files_dict[os.path.basename(i)]["score"]=int(files_dict[os.path.basename(i)]["score"])+int(overall_score)
+        tqdm.write("70% Completed") 
 
 
-#Unsual message behaviour (15 points)
+        '''Unsual message behaviour (15 points)'''
+        tqdm.write("Checking whether the Email was sent in unusual time")
         time=find_sending_time.extract_sending_time(i)
         hour = time.hour
         if 22 < hour or (0 <= hour < 8):
                 print("Phishy Time Detected:")
                 overall_score=10
+                files_dict[os.path.basename(i)]["details"]["Sending_Time"] = "Unusual"
         else:
                 print("Safe Time:")
                 overall_score=0
-        files_dict[os.path.basename(i)]=overall_score
+                files_dict[os.path.basename(i)]["details"]["Sending_Time"] = "Normal"
+        files_dict[os.path.basename(i)]["score"]=int(files_dict[os.path.basename(i)]["score"])+int(overall_score)
+        tqdm.write("80% Completed") 
 
         
 
-#Reply to Field (10 points)
+        '''Reply to Field (10 points)'''
+        tqdm.write("Checking whether the replyt-to email-id and sender emai-id is same")
         reply_id=find_reply_id.read_files_and_find_reply_id(i)
         sender_id=find_from_email.read_files_and_find_from_email(i)
         if reply_id == sender_id:
                 print("not phisy")
                 overall_score=10
+                files_dict[os.path.basename(i)]["details"]["Mismatch_btw_reply-to_and_sender"] = "Yes"
         else:
                 print("phisy")
                 overall_score=0
-        files_dict[os.path.basename(i)]=overall_score
+                files_dict[os.path.basename(i)]["details"]["Mismatch_btw_reply-to_and_sender"] = "No"
+        files_dict[os.path.basename(i)]["score"]=int(files_dict[os.path.basename(i)]["score"])+int(overall_score)
+        tqdm.write("85% Completed") 
         
-# #IP reputation of the sender-ipadress (10 points)
-        
+        '''IP reputation of the sender-ipadress (10 points)'''
+        tqdm.write("Checking Reputation of the Sender-IP adress")
         sender_ip=find_sender_ip.read_files_and_find_sender_ip(i)
         print(sender_ip)
         url = 'https://api.abuseipdb.com/api/v2/check'
@@ -163,17 +267,32 @@ for i in files_path.keys():
         }
         res=requests.request(method='GET', url=url, headers=headers, params=querystring)
         respone=(json.loads(res.text))
-        score=(10-(respone["abuseConfidenceScore"]/10))
-        files_dict[os.path.basename(i)]=score
+        # print(respone["data"])
+        ipAdress={"ip-adress":respone["data"]["ipAddress"],
+                  "isPublic":respone["data"]["isPublic"],
+                  "Is_It_Whitelisted":respone["data"]["isWhitelisted"],
+                  "Risk_Score":respone["data"]["abuseConfidenceScore"],
+                  "Usage_Type":respone["data"]["usageType"],
+                  "Domain_name":respone["data"]["domain"],
+                  "Is_it_Tor":respone["data"]["isTor"],
+                  "Total_Reports_previous":respone["data"]["totalReports"],
+                  "Last_report_date":respone["data"]["lastReportedAt"]
+                  }
+        files_dict[os.path.basename(i)]["details"]["IP_adress_details"] = ipAdress
+        score=(10-(respone["data"]["abuseConfidenceScore"]/10))
+        files_dict[os.path.basename(i)]["score"]=10
+        files_dict[os.path.basename(i)]["score"]=int(files_dict[os.path.basename(i)]["score"])+int(score)
 
-#IP reputation of the sender-impts-ip (10 points)
-        
+
+        '''IP reputation of the sender-impts-ip (10 points) '''
+        tqdm.write("Checking Reputation of Sender IMPTS-IP")
         sender_smtps__ip=find_smpts_sender_domain.read_files_and_find_smpts_server_domain(i)
         url1=sender_smtps__ip.strip(';')
         print(url1)
         encoded_url1=quote_plus(url1)
         res=requests.get(f"https://www.ipqualityscore.com/api/json/url/{os.environ.get('URL_VALIDATION')}/{encoded_url1}")
         response=(json.loads(res.content))
+        print(response)
         scores = {
         #    "success": 2 if response["success"] else -4,
         #     "unsafe": -4 if response["unsafe"]else 2,
@@ -185,5 +304,22 @@ for i in files_path.keys():
         #     "suspicious": -4 if response["suspicious"] else 2,
                 "risk_score":float(25-(response["risk_score"]/4))
         }
+
+        ipAdress={"Safe":"Yes" if response["unsafe"] else "No",
+                  "Domain_name":response["domain"],
+                  "IP_address":response["ip_address"],
+                  "server_name":response["server"],
+                  "dns_valid":"Yes" if response["dns_valid"]else "No",
+                  "Domain_name":response["domain"],
+                  "spamming":response["spamming"],
+                  "malware":response["malware"],
+                  "phishing":response["phishing"],
+                  "suspicious":response["suspicious"],
+                  "malicious_score":response["risk_score"]
+                  }
+        files_dict[os.path.basename(i)]["details"]["IMTPS_IP_adress_details"] = ipAdress
         overall_score = sum(scores[key] for key in scores.keys())
-        files_dict[os.path.basename(i)]=overall_score
+        files_dict[os.path.basename(i)]["score"]=int(files_dict[os.path.basename(i)]["score"])+int(overall_score)
+        tqdm.write("100% Completed") 
+
+print(json.dumps(files_dict, indent=2))
